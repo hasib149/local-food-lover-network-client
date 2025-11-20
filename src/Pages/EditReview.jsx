a8b82225b6fcc27603d881daba043d092642c2b2
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-11-20 13:45:35 +0600

    edit page done

--- src/Pages/EditReview.jsx
+++ src/Pages/EditReview.jsx
@@ -26,7 +26,7 @@ const EditReview = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log("Successfully saved:", data);
-        toast.success("Your review has been submitted successfully.")
+        toast.success("Your review has been submitted successfully.");
         navigate("/my-review");
       })
 
@@ -36,34 +36,36 @@ const EditReview = () => {
   };
   return (
     <div className="p-6 max-w-md mx-auto">
-      <h2 className="text-2xl font-semibold mb-4">Edit Review</h2>
+      <h2 className="text-3xl text-green-700 font-semibold mb-4">
+        Edit Review
+      </h2>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           name="foodName"
           defaultValue={review.foodName}
           placeholder="Food Name"
-          className="p-2 border rounded"
+          className="p-2 border rounded focus:border-green-600 focus:ring-2 focus:ring-green-200 outline-none"
         />
         <input
           type="text"
           name="restaurantName"
           defaultValue={review.restaurantName}
           placeholder="Restaurant Name"
-          className="p-2 border rounded"
+          className="p-2 border rounded focus:border-green-600 focus:ring-2 focus:ring-green-200 outline-none"
         />
         <input
           type="text"
           name="foodImage"
           defaultValue={review.foodImage}
           placeholder="Food Image URL"
-          className="p-2 border rounded"
+          className="p-2 border rounded focus:border-green-600 focus:ring-2 focus:ring-green-200 outline-none"
         />
         <input
           type="text"
           name="reviewDate"
           defaultValue={review.reviewDate}
-          className="p-2 border rounded"
+          className="p-2 border rounded focus:border-green-600 focus:ring-2 focus:ring-green-200 outline-none"
         />
         <button
           type="submit"
