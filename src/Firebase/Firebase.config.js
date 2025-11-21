9ffbaf0806298ed41a13046040cd1166358dae7b
Hasib Shrabon <hasibahmedshrabon@gmail.com>
2025-11-21 12:42:37 +0600

    .env file done

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC8SJJWCifM1Ng6jUWMhVWVSe3-oib5rFs",
-  authDomain: "local-food-review.firebaseapp.com",
-  projectId: "local-food-review",
-  storageBucket: "local-food-review.firebasestorage.app",
-  messagingSenderId: "985766373376",
-  appId: "1:985766373376:web:aebf1a64d8410ba99a90e5"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
